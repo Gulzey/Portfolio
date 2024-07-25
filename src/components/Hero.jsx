714b21db8d5f2784318e1957d49184479db4ce0d
Gulzey <Gxlz4418@gmail.com>
2024-07-25 02:15:23 +0100

    xyz

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,14 @@
 import { motion } from 'framer-motion';
-
 import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px]
-      max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
-          <div className='w-1 sm:h-80 violet-gradient'/>
-
+          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Guled</span></h1>
@@ -20,27 +17,25 @@ const Hero = () => {
       </div> 
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 w-full flex justify-center items-center'>
+      <div className='absolute w-full flex justify-center items-center' style={{ bottom: '10vh' }}>
         <a href='#about'>
-        <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-         <motion.dev
-         animate={{
-          y: [0, 24, 0]
-         }}
-         transition={{
-          duration: 1.5,
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
                 repeat: Infinity,
                 repeatType: "loop",
               }}
               className='w-3 h-3 rounded-full bg-secondary mb-1'
             />
           </div>
-
         </a>
-
       </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
